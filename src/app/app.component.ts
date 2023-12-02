fab3daa09bcc159c889a98cf65c6e13ff295fd9f
muradjpr <muradjpr@gmail.com>
2023-12-03 00:42:43 +0600

    after login and product search and detail page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Inject, PLATFORM_ID, OnInit } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'mycart17';
+export class AppComponent implements OnInit {
+  static isBrowser = new BehaviorSubject<any>(null);
+
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {
+    AppComponent.isBrowser.next(isPlatformBrowser(platformId));
+
+
+  }
+
+  ngOnInit(): void {
+
+
+  }
 }
